3530059a2acee63287a45442044830c95a32a6fe
Helen Miller <helen@Baker.domain>
2017-06-19 16:33:53 -0700

    Added some summary stats for year built and visualizations for PA

--- localExamples/Helen.R
+++ localExamples/Helen.R
@@ -4,21 +4,44 @@
 
 source("R/convert2016.R")
 nbi <- convert2016("~/Data/2016hwybronlyonefile.zip")
-nbi$yearBuilt <- as.numeric(rawDF$YEAR_BUILT_027)
+
+#When were bridges built?
+hist(nbi$yearBuilt, main = "Year Built",n=50)
+
+#Center it around 1900
+nbi$yearSince1900 <- nbi$yearBuilt-1900
+
+#How do bridge ages differ between different states?
+yearBuiltMeans <- aggregate(data = nbi, yearSince1900~stateCode, FUN = mean)
+yearBuiltMedians <- aggregate(data = nbi, yearSince1900~stateCode, FUN = median)
+#How can I visualize these results?
 
 # Make a plot of Pennsylvania with bridges colored by year
 
-#Divide year made into groups
+#Index years so bridges can be colored by year made
 breaks <- c(-Inf, seq(1900, 2020, by=20))
 colorIndex <- .bincode(nbi$yearBuilt, breaks=breaks)
-#Define colors for ages 
+nbi$colorIndex <- colorIndex
+#Define colors for each year
 library(RColorBrewer) 
 myColors <- brewer.pal(7, "Spectral")
 #draw the map
 map("state", "Penn")
 pa <- subset(nbi, stateCode == "PA")
-points(pa$longitude, pa$latitude, pch=17, cex=.5, col=myColors[colorIndex])
+points(pa$longitude, pa$latitude, pch=17, cex=(3.5-pa$colorIndex/2), col=myColors[pa$colorIndex])
 map("county", add=TRUE)
 legend("topright", horiz=TRUE, legend  = breaks, fill = myColors)
+
+#Where are the oldest bridges?
+oldPa <- subset(pa, yearBuilt <= 1900)
+map("state", "Penn")
+points(oldPa$longitude, oldPa$latitude, pch = 17, cex = .5)
+
+#Where are the newest bridges?
+newPa <- subset(pa, yearBuilt >= 2000)
+map("state", "Penn")
+points(newPa$longitude, newPa$latitude, pch = 17, cex = .5)
+
+
 
 
