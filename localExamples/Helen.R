9a101f6245b6c2e9a6c409540bfb724b02f8d4fd
Helen Miller <helen@Baker.domain>
2017-06-23 11:10:33 -0700

    Map of states colored by bridge age

--- localExamples/Helen.R
+++ localExamples/Helen.R
@@ -27,32 +27,44 @@ stateBreaks <- seq(30,60,by=5)
 meanAgeIndex <- .bincode(meanAgeByState$age, breaks = stateBreaks)
 
 # 2. create color pallete
-stateColors <- brewer.pal(6,"PuRd")
+stateColors <- RColorBrewer::brewer.pal(6,"PuRd")
 
 # 3. get state names associated with stateCodes
 df <- maps::state.fips
-df$stateName <- str_extract(df$polyname, "[^:]+")
+df$stateName <- stringr::str_extract(df$polyname, "[^:]+")
 df1 <- df[!duplicated(df$stateName), ]
 nameByCode <- as.character(df1$stateName)
 names(nameByCode) <- as.character(df1$abb)
 
-# 4. plot states, filled in by color index, corresponding to the color pallete from part 2.
-# # For some reason, this does not color the states correctly...
-# map("state", nameByCode[meanAgeByState$stateCode], fill = TRUE, col = stateColors[meanAgeByState$Index])
-# legend("bottomleft", legend = stateBreaks, pch = 15, col = stateColors[1:8])
-
-# # This will map the states with the correct colors. However, it is not ideal...
-#
-# meanAgeByState$Index <- meanAgeIndex
-# map("state", nameByCode[filter(meanAgeByState, Index == 1)$stateCode], fill = TRUE, col = stateColors[1], add= TRUE)
-# map("state", nameByCode[filter(meanAgeByState, Index == 2)$stateCode], fill = TRUE, col = stateColors[2], add= TRUE)
-# map("state", nameByCode[filter(meanAgeByState, Index == 3)$stateCode], fill = TRUE, col = stateColors[3], add= TRUE)
-# map("state", nameByCode[filter(meanAgeByState, Index == 4)$stateCode], fill = TRUE, col = stateColors[4], add= TRUE)
-# map("state", nameByCode[filter(meanAgeByState, Index == 5)$stateCode], fill = TRUE, col = stateColors[5], add= TRUE)
-# map("state", nameByCode[filter(meanAgeByState, Index == 6)$stateCode], fill = TRUE, col = stateColors[6], add= TRUE)
-# legend("bottomleft", legend = stateBreaks, pch = 15, col = stateColors[1:8])
+names(meanAgeIndex) <- meanAgeByState$stateCode
 
 
+# # 4. plot states, filled in by color index, corresponding to the color pallete from part 2.
+#  # For some reason, this does not color the states correctly...
+#  map("state", nameByCode[meanAgeByState$stateCode], fill = TRUE, col = stateColors[meanAgeIndex])
+#  legend("bottomleft", legend = stateBreaks, pch = 15, col = stateColors[1:8])
+# # # This will map the states with the correct colors. However, it is not ideal...
+#  meanAgeByState$Index <- meanAgeIndex
+#  map("state", nameByCode[dplyr::filter(meanAgeByState, Index == 1)$stateCode], fill = TRUE, col = stateColors[1], add= TRUE)
+#  map("state", nameByCode[dplyr::filter(meanAgeByState, Index == 2)$stateCode], fill = TRUE, col = stateColors[2], add= TRUE)
+#  map("state", nameByCode[dplyr::filter(meanAgeByState, Index == 3)$stateCode], fill = TRUE, col = stateColors[3], add= TRUE)
+#  map("state", nameByCode[dplyr::filter(meanAgeByState, Index == 4)$stateCode], fill = TRUE, col = stateColors[4], add= TRUE)
+#  map("state", nameByCode[dplyr::filter(meanAgeByState, Index == 5)$stateCode], fill = TRUE, col = stateColors[5], add= TRUE)
+#  map("state", nameByCode[dplyr::filter(meanAgeByState, Index == 6)$stateCode], fill = TRUE, col = stateColors[6], add= TRUE)
+#  legend("bottomleft", legend = stateBreaks, pch = 15, col = stateColors[1:8])
+
+#Now, do the same thing but with MazamaSpatialUtils
+library(MazamaSpatialUtils)
+us <- subset(NaturalEarthAdm1, countryCode == "US")
+conusID <- us$stateCode
+conusID <- setdiff(conusID, c("AK", "HI"))
+conus <- subset(us, stateCode %in% conusID)
+
+plot(conus, col = stateColors[meanAgeIndex[conus$stateCode]])
+legend("bottomleft", legend = c("30-35 yrs", "35-40 yrs", "40-45 yrs", "45-50 yrs", "50-55 yrs", "55-60 yrs"), 
+       pch = 15, col = stateColors[1:6], title = "Mean Bridge Age")
+title("Mean Bridge Age")
+ 
 # Make a plot of Pennsylvania with bridges colored by year
 
 #Index years so bridges can be colored by year made
@@ -63,6 +75,7 @@ nbi$colorIndex <- colorIndex
 library(RColorBrewer) 
 myColors <- brewer.pal(7, "Spectral")
 #draw the map
+library(maps)
 map("state", "Penn")
 pa <- subset(nbi, stateCode == "PA")
 points(pa$longitude, pa$latitude, pch=17, cex=(3.5-pa$colorIndex/2), col=myColors[pa$colorIndex])
