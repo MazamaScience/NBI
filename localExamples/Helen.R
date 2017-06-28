0e4dd7de656f8b789d5b7fca3a534a80f5a830be
Helen Miller <helen@Baker.domain>
2017-06-28 15:56:14 -0700

    mode year built plots

--- localExamples/Helen.R
+++ localExamples/Helen.R
@@ -9,6 +9,7 @@ nbi <- convert2016("~/Data/2016hwybronlyonefile.zip")
 nbi$age <- 2016 - nbi$yearBuilt
 meanAgeByState <- aggregate(data = nbi, age ~ stateCode, FUN = mean)
 medianAgeByState <- aggregate(data = nbi, age ~ stateCode, FUN = median)
+modeAgeByState <- aggregate(data = nbi, age ~ stateCode , FUN = mode)
 
 orderedAge <- meanAgeByState[order(meanAgeByState$age),]
 orderedMedian <- medianAgeByState[order(medianAgeByState$age),]
@@ -58,6 +59,46 @@ legend("bottomleft", legend = c("30-35 yrs", "35-40 yrs", "40-45 yrs", "45-50 yr
        pch = 15, col = stateColors[1:6], title = "Mean Bridge Age")
 title("Mean Bridge Age")
  
+#In which decade were most bridges built in each state?
+getmode <- function(v) {
+uniqv <- unique(v)
+uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+modeAgeByState <- aggregate(data = nbi, yearBuilt ~ stateCode , FUN = getmode)
+
+
+
+decadeBreaks <- c(-Inf, seq(1800, 2020, by = 10))
+decadeBuilt <- .bincode(nbi$yearBuilt, breaks = decadeBreaks)
+decadeBuilt <- c("<1800", as.character(seq(1800,2020,by = 10)))[decadeBuilt]
+modeDecadeByState <- aggregate(decadeBuilt ~ nbi$stateCode , FUN = getmode)
+modeDecadeByState$decadeBuilt <- as.numeric(modeDecadeByState$decadeBuilt)
+modeAgeIndex <- .bincode(modeDecadeByState$decadeBuilt, breaks = seq(1929,1999, by = 10))
+names(modeAgeIndex) <- modeDecadeByState$`nbi$stateCode`
+decadeColors <- brewer.pal(7, "PuRd")
+
+plot(conus, col = decadeColors[modeAgeIndex[conus$stateCode]])
+legend("bottomleft", c("1930s", "1940s", "1950s", "1960s", "1970s", "1980s", "1990s"), pch = 15, col = decadeColors)
+
+
+
+#############################################################################
+
+decadeBreaks <- c(-Inf, seq(1800, 2020, by = 10))
+decadeBuilt <- .bincode(nbi$yearBuilt, breaks = decadeBreaks)
+decadeBuilt <- c("<1800", as.character(seq(1800,2020,by = 10)))[decadeBuilt]
+modeDecadeByState <- aggregate(decadeBuilt ~ nbi$stateCode , FUN = getmode)
+modeDecadeByState$decadeBuilt <- as.numeric(modeDecadeByState$decadeBuilt)
+modeAgeIndex <- .bincode(2000-modeDecadeByState$decadeBuilt, breaks = seq(9,79, by = 10))
+names(modeAgeIndex) <- modeDecadeByState$`nbi$stateCode`
+decadeColors <- RColorBrewer::brewer.pal(7, "PuRd")
+
+plot(conus, col = decadeColors[modeAgeIndex[conus$stateCode]])
+legend("bottomleft", c("1930s", "1940s", "1950s", "1960s", "1970s", "1980s", "1990s"), pch = 15, col = decadeColors[7:1])
+
+
+
+#############################################################################
 # Make a plot of Pennsylvania with bridges colored by year
 
 #Index years so bridges can be colored by year made
@@ -123,35 +164,35 @@ barplot(orderedTrafficDensity$averageCarCount, horiz = TRUE ,  las = 1, main = "
 
 
 #Multi-lane bridges
-summary(nbi$laneCount)
-summary(nbi$underLaneCount)
-#most bridges have two lanes and pass over 0 lanes.
-#what is up with bridges with 82 lanes, or passing over 99 lanes? 
+# summary(nbi$laneCount)
+# summary(nbi$underLaneCount)
+# #most bridges have two lanes and pass over 0 lanes.
+# #what is up with bridges with 82 lanes, or passing over 99 lanes? 
+# 
+# table(nbi$laneCount)
+# table(nbi$underLaneCount)
+# 
+# bigBridges <- nbi$laneCount > 8
+# reallyBigBridges <- nbi$laneCount > 20 
 
-table(nbi$laneCount)
-table(nbi$underLaneCount)
-
-bigBridges <- nbi$laneCount > 8
-reallyBigBridges <- nbi$laneCount > 20 
-
-map("state")
-points(nbi$longitude[bigBridges], nbi$latitude[bigBridges], cex = nbi$laneCount[bigBridges]/15, pch = 2)
-#Big bridges tend to be really clumped together. Most likely around big cities.
-map("state")
-points(nbi$longitude[reallyBigBridges], nbi$latitude[reallyBigBridges], cex = nbi$laneCount[reallyBigBridges]/25, pch = 2)
-#What is going on in Tennessee?
-
-#It looks like big bridges might follow similar patterns to the high-traffic bridges. Let's see.
-map("state")
-points(nbi$longitude, nbi$latitude, pch = 17, cex = as.numeric(nbi$averageCarCount)/807000*6)
-points(nbi$longitude[bigBridges], nbi$latitude[bigBridges], cex = nbi$laneCount[bigBridges]/15, pch = 2, col = "red")
-
-
-#There are a lot of really HUGE bridges in TN? Why? Let's see what's going on there. 
-tn <- dplyr::filter(nbi, stateCode == "TN")
-map("state", "ten")
-points(tn$longitude, tn$latitude, pch = 17, cex = as.numeric(tn$averageCarCount)/807000*6)
-points(nbi$longitude[bigBridges], nbi$latitude[bigBridges], cex = nbi$laneCount[bigBridges]/15, pch = 2, col = "red")
+# map("state")
+# points(nbi$longitude[bigBridges], nbi$latitude[bigBridges], cex = nbi$laneCount[bigBridges]/15, pch = 2)
+# #Big bridges tend to be really clumped together. Most likely around big cities.
+# map("state")
+# points(nbi$longitude[reallyBigBridges], nbi$latitude[reallyBigBridges], cex = nbi$laneCount[reallyBigBridges]/25, pch = 2)
+# #What is going on in Tennessee?
+# 
+# #It looks like big bridges might follow similar patterns to the high-traffic bridges. Let's see.
+# map("state")
+# points(nbi$longitude, nbi$latitude, pch = 17, cex = as.numeric(nbi$averageCarCount)/807000*6)
+# points(nbi$longitude[bigBridges], nbi$latitude[bigBridges], cex = nbi$laneCount[bigBridges]/15, pch = 2, col = "red")
+# 
+# 
+# #There are a lot of really HUGE bridges in TN? Why? Let's see what's going on there. 
+# tn <- dplyr::filter(nbi, stateCode == "TN")
+# map("state", "ten")
+# points(tn$longitude, tn$latitude, pch = 17, cex = as.numeric(tn$averageCarCount)/807000*6)
+# points(nbi$longitude[bigBridges], nbi$latitude[bigBridges], cex = nbi$laneCount[bigBridges]/15, pch = 2, col = "red")
 
 
 
@@ -203,6 +244,8 @@ with(dplyr::filter(nbi, water == 1), points(longitude, latitude, pch = 17, cex =
 map("state", "neb")
 with(dplyr::filter(nbi, water == 1 & stateCode == "NE"), points(longitude, latitude, pch = 2, cex = .2))
 with(dplyr::filter(nbi, water == 0 & stateCode == "NE"), points(longitude, latitude, pch = 2, cex = .5, col = "red"))
+
+
 
 
 map("state", "minne")
